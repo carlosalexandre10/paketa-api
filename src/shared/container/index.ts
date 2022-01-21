0dfa8ed695b3a3a752f66eb40e5620891ceefc2b
carlosalexandre10 <carlos11alexandre11@gmail.com>
2022-01-21 16:29:29 -0300

    Projeto Paketa

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,14 @@
 import { container } from 'tsyringe';
 
 import '@modules/users/providers';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 container.registerSingleton<IUsersRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
+);
